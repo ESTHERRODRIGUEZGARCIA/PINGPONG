869546619ec4eef13454de75e5dc21e97e2e2dd1
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-30 18:42:10 +0100

    metodo rebotar

--- pingpong_grupal.py
+++ pingpong_grupal.py
@@ -34,6 +34,18 @@ class PelotaPong:
         self.x += self.dir_x
         self.y += self.dir_y
 
+    def rebotar(self):
+        if self.x <= 0:
+            self.dir_x = -self.dir_x
+        if self.x + self.ancho >= VENTANA_HORI:
+            self.dir_x = -self.dir_x
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+
+
+
 def main():
     # Inicialización de Pygame
     pygame.init()
