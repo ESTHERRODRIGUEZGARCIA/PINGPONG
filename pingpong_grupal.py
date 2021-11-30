7b52769f14a292db628f874134f3bd7346b2d007
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-30 18:23:19 +0100

    realizando superficie del dibujo

--- pingpong_grupal.py
+++ pingpong_grupal.py
@@ -5,3 +5,8 @@
 import pygame 
 from pygame.locals import QUIT
 
+# constantes para la superficie del dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
