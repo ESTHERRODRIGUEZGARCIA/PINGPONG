299a05b337a57dd68b4d40f2a3203e0aee6a519c
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-01 20:02:50 +0100

    texto

--- pingpong_grupal.py
+++ pingpong_grupal.py
@@ -146,6 +146,10 @@ def main():
         ventana.blit(raqueta_1.imagen, (raqueta_1.x, raqueta_1.y))
         ventana.blit(raqueta_2.imagen, (raqueta_2.x, raqueta_2.y))
 
+        texto = f"{pelota.puntuacion} : {pelota.puntuacion_ia}"
+        letrero = fuente.render(texto, False, NEGRO)
+        ventana.blit(letrero, (VENTANA_HORI / 2 - fuente.size(texto)[0] / 2, 50))
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 jugando = False
