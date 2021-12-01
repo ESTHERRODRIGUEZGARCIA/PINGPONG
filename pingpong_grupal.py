d7c76d73da8144519b825b8db753fa5772cf4136
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-01 18:44:32 +0100

    commit

--- pingpong_grupal.py
+++ pingpong_grupal.py
@@ -12,26 +12,25 @@ VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
 NEGRO = (0,0,0)
-#definimos clase y características 
+
+#definimos clase y caracteristicas
 class PelotaPong:
     def __init__(self, fichero_imagen):
-        
-        # Imagen
+        # --- Atributos de la Clase ---
+
+        # Imagen de la Pelota
         self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
 
-        # Dimensiones
+        # Dimensiones de la Pelota
         self.ancho, self.alto = self.imagen.get_size()
-        #posición
+
+        # Posición de la Pelota
         self.x = VENTANA_HORI / 2 - self.ancho / 2
         self.y = VENTANA_VERT / 2 - self.alto / 2
 
         # Dirección de movimiento de la Pelota
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
-
-        # Puntuación de la pelota
-        self.puntuacion = 0
-        self.puntuacion_ia = 0
 
     def mover(self):
         self.x += self.dir_x
@@ -46,12 +45,13 @@ class PelotaPong:
             self.dir_y = -self.dir_y
         if self.y + self.alto >= VENTANA_VERT:
             self.dir_y = -self.dir_y
-    
+
     def reiniciar(self):
         self.x = VENTANA_HORI / 2 - self.ancho / 2
         self.y = VENTANA_VERT / 2 - self.alto / 2
         self.dir_x = -self.dir_x
-        self.dir_y = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])        
+
 
 
 def main():
