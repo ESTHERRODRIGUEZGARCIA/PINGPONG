ffe12bb2edd2a42ef162e2714744e89a7de901bc
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-01 12:52:09 +0100

    Añadimos "negro"

--- pingpong_grupal.py
+++ pingpong_grupal.py
@@ -11,11 +11,11 @@ VENTANA_HORI = 800  # Ancho de la ventana
 VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
+NEGRO = (0,0,0)
 
 class PelotaPong:
     def __init__(self, fichero_imagen):
-        # --- Atributos de la Clase ---
-
+        
         # Imagen de la Pelota
         self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
 
